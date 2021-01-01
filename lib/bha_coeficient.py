2f3d606b94fb51179aece796f681e83b45aad7a7
luc <duongduyluc3@gmail.com>
2021-01-01 23:08:49 +0700

    sua toi ngay 1-1-2021

--- lib/bha_coeficient.py
+++ lib/bha_coeficient.py
@@ -4,31 +4,40 @@ import time
 import os
 import sys
 class bhat_coe:
-    
-
-    def __init__(self, model_img, n_channel = 1, h_v = None, s_v = None, v_v = None, n_bin):
+    def __init__(self,frame, model_img, n_channel = 1, h_v = None, s_v = None, v_v = None, n_bin = 180):
+        self.model_img = model_img.copy()
+        self.frame_shape = frame.shape
         if n_channel < 0:
             print("so channel phai lon hon 0")
             sys.exit()
         if n_channel == 1:
             self.v_range = h_v
             self.channel = [0]
-        if n_channel == 2:
+        elif n_channel == 2:
             self.v_range = h_v + s_v
-            self.channal = [0,1]
+            self.channel = [0,1]
         else:
             self.v_range = h_v + s_v + v_v
-            self.channal = [0,1,2]
-        self.bin = n_bin
-        preprocess()
+            self.channel = [0,1,2]
+        self.bin = [n_bin]
+        self.preprocess()
 
     def preprocess(self):
-        self.model_hsv = cv.cvtColor(self.model_img,cv.COLOR_BGR_HSV)
-        self.model_hst = cv.calcHist(model_hsv,self.channal,None,self.bin,self.v_range)
+        self.model_hsv = cv.cvtColor(self.model_img,cv.COLOR_BGR2HSV)
+        self.model_hst = cv.calcHist(self.model_hsv,self.channel,None,self.bin,self.v_range)
     def make_window(self,pso,window):
         self.candidate = np.ones((self.n_pso,4),dtype = np.uint8)
-        self.candidate[:,0:2] = pso - window[2:4]
-        self.candidate[:,2:4] = self.candidate[0,0:4]*window[2:4]
-        if any
+        self.candidate[:,0:2] = pso - (np.copy(window[2:4])/2)
+        self.candidate[:,2:4] = self.candidate[:,2:4]*window[2:4]
+        if np.any(self.candidate < 0):
+            self.candidate[np.where(self.candidate < 0)] = 0
+            print("case 1")
+        if np.any(self.candidate[:,0] > self.frame_shape[1]-1):
+            self.candidate[np.where(self.candidate[:,0] > self.frame_shape[1]-1),0] = self.frame_shape[1]
+            print("case 2")
+        if np.any(self.candidate[:,1] > self.frame_shape[0]-1):
+            self.candidate[np.where(self.candidate[:,1] > self.frame_shape[0]-1),1] = self.frame_shape[0]
+            print("case 3")
     def make_data(self,frame,window):
+        pass
 
--- test.py
+++ test.py
@@ -1,5 +1,28 @@
+import cv2 as cv
 import numpy as np
-a = [0,1]
-b = [2,3]
-c = a+b
-print(c)
+from lib.bha_coeficient import bhat_coe
+import time
+import os
+import sys
+
+class test(bhat_coe):
+    def __init__(self,frame,model_img,window,n_channel=None,h_v=None,s_v=None,v_v=None,n_bin=None):
+        super().__init__(frame,model_img,n_channel,h_v,s_v,v_v,n_bin)
+        center = np.concatenate((np.random.randint(0,100,(10,1)),np.random.randint(0,100,(10,1))),axis=1)
+        center = np.array([[1,100]])
+        self.n_pso = 1
+        self.make_window(center,window)
+        with open("data.txt","w") as file:
+            file.write(str(window))
+            file.write(str(self.candidate))
+
+path = "../besave.jpg"
+img = cv.imread(path)
+cv.imshow("img",img)
+window = cv.selectROI(img,fromCenter = None)
+cv.destroyWindow("select ROI")
+model = img[window[1]:window[1]+window[3],window[0]:window[0]+window[2]]
+print("________________________")
+my_distance = test(img,model,window,1,[0,180],n_bin = 30)
+cv.imshow("roi",model)
+cv.waitKey(0)
